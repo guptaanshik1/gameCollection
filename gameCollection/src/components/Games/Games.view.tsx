35f5afa8d36b8862582808641e1a50c2118ca783
Anshik <guptaanshik1@gmail.com>
2023-09-17 16:18:42 +0530

    removed one extra annotation

--- gameCollection/src/components/Games/Games.view.tsx
+++ gameCollection/src/components/Games/Games.view.tsx
@@ -1,6 +1,5 @@
 import { useGamesContext } from "./utils/context";
 import GameCard from "./components/GameCard";
-import { IAllGameResult } from "../../data/AllGame";
 import { SimpleGrid } from "@chakra-ui/react";
 
 export default function GamesView() {
@@ -9,7 +8,7 @@ export default function GamesView() {
   return (
     <>
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} p={"10px"} spacing={5}>
-        {games?.map((game: IAllGameResult) => (
+        {games?.map((game) => (
           <GameCard game={game} />
         ))}
       </SimpleGrid>
